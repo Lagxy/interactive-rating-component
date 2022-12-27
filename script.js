69bde2013f1a5660ee2399051101f064a64da32e
Lagxy <justtrashcount@gmail.com>
2022-12-27 16:34:09 +0700

    Done

---
+++ script.js
@@ -0,0 +1,11 @@
+const submit = document.querySelector(".submit");
+const content1 = document.querySelector('.content');
+const content2 = document.querySelector('.content2');
+const text = document.querySelector('.circle2');
+
+submit.addEventListener('click', () => {
+    const value = document.querySelector('input[type="radio"]:checked');
+    content1.style.display = "none";
+    content2.style.display = "flex";
+    text.textContent = `You selected ${value.value} out of 5`;
+})
